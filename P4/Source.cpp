f0de2fa319dc8a24c2d35cb1db0a3858c69e8ce6
rcham.pro <rcham.pro@gmail.com>
2020-11-23 16:25:37 +0100

    Function Show

--- P4/Source.cpp
+++ P4/Source.cpp
@@ -3,29 +3,54 @@
 
 using namespace std;
 
-int main() {
-	srand(time(NULL));
-	int tab[6][7] = { 0 };
-	for (int i = 0; i < 6; i++) {
-		for (int j = 0; j < 7; j++) {
-			tab[i][j] = (rand() % 2) + 1;
-		}
-	}
+void show(int a[6][7]) {
 	for (int i = 0; i < 6; i++)
 	{
-		std::cout << "| ";
+		cout << "| ";
 		for (int j = 0; j < 7; j++)
 		{
-			if (tab[i][j] == 0)
+			switch (a[i][j]) {
+			case 0:
 				cout << " ";
-			if (tab[i][j] == 1)
+				break;
+			case 1:
 				cout << "X";
-			if (tab[i][j] == 2)
+				break;
+			case 2:
 				cout << "O";
+				break;
+			default:
+				break;
+			}
 			cout << " ";
 		}
 		cout << "|" << endl << "|---------------|" << endl;
 	}
-	cout << "  0 1 2 3 4 5 6  " << endl << endl;
+	cout << "  1 2 3 4 5 6 7  " << endl << endl;
+}
+
+int main() {
+	srand(time(NULL));
+	int tab[6][7] = { 0 };
+	show(tab);
+
+	//Test random
+	/*for (int i = 0; i < 6; i++) {
+		for (int j = 0; j < 7; j++) {
+			tab[i][j] = (rand() % 2) + 1;
+		}
+	}*/
+	int colonne;
+	while (1) {
+		cin >> colonne;
+		colonne--;
+		for (int i = 5; i >= 0; i--) {
+			if (tab[i][colonne] == 0) {
+				tab[i][colonne] = 1;
+				break;
+			}
+		}
+		show(tab);
+	}
 	return 0;
 }
